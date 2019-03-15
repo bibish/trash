64fa4a9c66083d3aa678adcdb94d58873ed55c1f
Ambroise <amb.deyna@gmail.com>
2019-03-15 08:44:56 +0100

    Start ex

---
+++ Ex.py
@@ -0,0 +1,9 @@
+class definePrice:
+  def __init__(self, price, weight, format, type,):
+    self,price = price
+    self.weight = weight
+    self.format = format
+    self.type = type
+    
+  def letterPrice: 
+    
