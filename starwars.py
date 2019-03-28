0355ee76bba31efc49ca915c35a1112902c127c7
Ambroise <amb.deyna@gmail.com>
2019-03-28 15:32:00 +0100

    sqlite

--- starwars.py
+++ starwars.py
@@ -40,3 +40,21 @@ for elm in peoples:
             
 df = pd.DataFrame.from_dict(listOfClean)
 df.head()
+
+
+#exo sqlite
+1/ - sqlite3 chinook.db - .tables 2/ .tables '%s' 
+
+3/.schema albums 
+
+4/.fullschema 
+
+5/ select * from tracks; 
+
+6/select * from albums order by ArtistId limit 10; 
+
+7/select Name from tracks order by AlbumId limit 10; 
+
+8/select Name from tracks where AlbumId = 1 limit 10; 
+
+9/select Name from tracks where AlbumId = 1 and Milliseconds > 252980 limit 10 ; 10/select Name,MediaTypeId from tracks where MediaTypeId = 1 or MediaTypeId = 2 order by Name limit 10; select ar.ArtistId,Name, a.AlbumId from artists ar left join albums a on ar. ArtistId = a.ArtistId where ar.ArtistId = a.AlbumId order by AlbumId;
